0bccd91da2511eeea6dad0081c79fa745d4e07ae
Dawid Drozd <dawid.drozd@playsoft.fr>
2017-08-17 09:21:26 +0200

    Add documentation to ChangeOnEnd

--- src/acme/guard/ChangeOnEnd.h
+++ src/acme/guard/ChangeOnEnd.h
@@ -4,6 +4,19 @@
 
 #pragma once
 
+/**
+ * Also checkout std::lock_guard
+ * http://en.cppreference.com/w/cpp/thread/lock_guard
+ *
+ * C++ standard
+ *
+ * * 3.7.2/3
+ * If a named automatic object has initialization or a destructor with side effects, it shall not be destroyed before the end of its block, nor shall it be eliminated as an optimization even if it appears to be unused
+ *
+ *
+ * https://stackoverflow.com/questions/2087600/is-a-c-destructor-guaranteed-not-to-be-called-until-the-end-of-the-block
+ */
+
 template<class T, class V>
 class ChangeOnEnd
 {

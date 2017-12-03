3e7603c286a2e2d0a06aecff2c04b328333559bb
Dawid Drozd <drozddawid.uam@gmail.com>
2017-12-03 14:16:31 +0100

    Update Bezier

--- src/math/Bezier.h
+++ src/math/Bezier.h
@@ -3,6 +3,8 @@
 //
 #pragma once
 
+#include <cassert>
+
 #include <geometry/Point.h>
 
 namespace math
@@ -10,6 +12,16 @@ namespace math
 namespace Bezier
 {
 
+/**
+ * @brief https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+ * @tparam Type
+ * @param t , 0 ≤ t ≤ 1
+ * @param A first point (P0)
+ * @param B (P1)
+ * @param C (P2)
+ * @param D last point (P3)
+ * @return B(t) result
+ */
 template<typename Type>
 Dexode::Point<Type> cubic(const Type& t
 						  , const Dexode::Point<Type>& A
@@ -17,6 +29,8 @@ Dexode::Point<Type> cubic(const Type& t
 						  , const Dexode::Point<Type>& C
 						  , const Dexode::Point<Type>& D)
 {
+	assert(t <= 1 && t >= 0);
+
 	Dexode::Point<Type> result = A * (1 - t) * (1 - t) * (1 - t);//A(1-t)^3
 	result += 3 * B * t * (1 - t) * (1 - t);// 3Bt(1-t)^2
 	result += 3 * C * t * t * (1 - t);// 3Ct^2(1-t)

64cb878468e2282ecf7265376383523cccb5641e
Dawid Drozd <drozddawid.uam@gmail.com>
2017-12-03 14:17:52 +0100

    Add math::supports_arithmetic_operations traits

---
+++ src/math/traits.h
@@ -0,0 +1,25 @@
+//
+// Created by Dawid Drozd aka Gelldur on 03.12.17.
+//
+#pragma once
+
+#include <type_traits>
+
+namespace math
+{
+
+template<class T, class = void>
+struct supports_arithmetic_operations : std::false_type {};
+
+template<class T>
+struct supports_arithmetic_operations
+		<T
+		 , std::void_t<decltype(std::declval<T>() + std::declval<T>())
+					   , decltype(std::declval<T>() - std::declval<T>())
+					   , decltype(std::declval<T>() * std::declval<T>())
+					   , decltype(std::declval<T>() / std::declval<T>())>>
+		: std::true_type
+{
+};
+
+}

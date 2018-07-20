8b9a65a776e039bc0dc09a31f28ee0389dafd5a2
Dawid Drozd <drozddawid.uam@gmail.com>
2018-07-20 10:48:03 +0200

    Add StringUtils::binaryToHex helper
    Print binary data in hex format

--- src/acme/StringUtils.h
+++ src/acme/StringUtils.h
@@ -89,4 +89,21 @@ void replaceAll(std::string& inString, const std::string& fromWhat, const std::s
 
 std::string toLower(const std::string& text);
 
+///Print binary data in hex format string
+template<class Container> std::string binaryToHex(const Container& data)
+{
+	constexpr char hexmap[] = {
+	    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+	const auto size = std::distance(std::begin(data), std::end(data));
+
+	std::string text(size * 2, ' ');
+	for(int i = 0; i < size; ++i)
+	{
+		text[2 * i] = hexmap[(data[i] & 0xF0) >> 4];
+		text[2 * i + 1] = hexmap[data[i] & 0x0F];
+	}
+	return text;
+}
+
 }

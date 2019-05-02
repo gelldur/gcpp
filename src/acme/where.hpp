ccf3f2035f7ad5d4801338cb7611c426d7a4d1d3
Dawid Drozd <drozddawid.uam@gmail.com>
2019-05-02 09:49:08 +0200

    Update WHERE macro with extra spaces
    Add extra surrounding spaces. More convenient when making std::cout << WHERE << std::endl;

--- src/acme/where.hpp
+++ src/acme/where.hpp
@@ -8,7 +8,11 @@
 #define S2(x) S1(x)
 
 // WHERE - const char* const should be used as temporary value
-#define WHERE (string::make(__PRETTY_FUNCTION__) + ":" + string::make(S2(__LINE__))).get()
+// E.g. std::cout << WHERE << std::endl;
+#define WHERE                                                                                      \
+	(string::make(" ") + string::make(__PRETTY_FUNCTION__) + string::make(":") +                   \
+	 string::make(S2(__LINE__)) + string::make(" "))                                               \
+		.get()
 
 // It is safe to store e.g. `constexpr auto where = WHERE_STR;`
 #define WHERE_STR (string::make(__PRETTY_FUNCTION__) + ":" + string::make(S2(__LINE__)))

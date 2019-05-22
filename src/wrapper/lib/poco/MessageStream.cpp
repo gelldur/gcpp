6ba2d3e73beeb0fd48c4e5e5d6a25b48ec2d7342
Dawid Drozd <drozddawid.uam@gmail.com>
2019-05-22 23:32:14 +0200

    Update Poco::MessageStream

--- src/wrapper/lib/poco/MessageStream.cpp
+++ src/wrapper/lib/poco/MessageStream.cpp
@@ -6,18 +6,59 @@
 
 namespace Poco
 {
-MessageStream::MessageStream(const std::string& logger,
+
+MessageStream::MessageStream(std::string logger)
+
+	: _loggerName(std::move(logger))
+{}
+
+MessageStream::MessageStream(std::string logger,
 							 const std::string& source,
 							 Poco::Message::Priority prio,
 							 const char* file,
 							 int line)
 	: _message{source, "", prio, file, line}
-	, _loggerName(logger)
+	, _loggerName(std::move(logger))
 {}
 
 MessageStream::~MessageStream()
 {
 	send();
+}
+
+MessageStream& MessageStream::source(const std::string& sourceName)
+{
+	_message.setSource(sourceName);
+	return *this;
+}
+MessageStream& MessageStream::debug()
+{
+	_message.setPriority(Poco::Message::PRIO_DEBUG);
+	return *this;
+}
+MessageStream& MessageStream::information()
+{
+	_message.setPriority(Poco::Message::PRIO_INFORMATION);
+	return *this;
+}
+MessageStream& MessageStream::warning()
+{
+	_message.setPriority(Poco::Message::PRIO_WARNING);
+	return *this;
+}
+MessageStream& MessageStream::error()
+{
+	_message.setPriority(Poco::Message::PRIO_ERROR);
+	return *this;
+}
+MessageStream& MessageStream::where(std::string_view fileName, int line)
+{
+	_message.setSourceFile(fileName.data());
+	if(line != -1)
+	{
+		_message.setSourceLine(line);
+	}
+	return *this;
 }
 
 void MessageStream::send()
--- src/wrapper/lib/poco/MessageStream.hpp
+++ src/wrapper/lib/poco/MessageStream.hpp
@@ -15,13 +15,21 @@ namespace Poco
 class MessageStream : public std::stringstream
 {
 public:
-	MessageStream(const std::string& logger,
+	MessageStream(std::string logger);
+	MessageStream(std::string logger,
 				  const std::string& source,
 				  Poco::Message::Priority prio,
 				  const char* file,
 				  int line);
 
 	~MessageStream() override;
+
+	MessageStream& source(const std::string& sourceName);
+	MessageStream& debug();
+	MessageStream& information();
+	MessageStream& warning();
+	MessageStream& error();
+	MessageStream& where(std::string_view fileName, int line = -1);
 
 	/**
 	 * Sends message to logger. It is done automatically when dtor is called.

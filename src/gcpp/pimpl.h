0e8b89ec09d3158ad82684cd0a06ce8ddab5639a
Dawid Drozd <drozddawid.uam@gmail.com>
2019-12-22 00:02:50 +0100

    Add pimpl.h intention

--- src/gcpp/pimpl.h
+++ src/gcpp/pimpl.h
@@ -2,5 +2,15 @@
 
 #include <memory>
 
+/**
+ * This isn't in any namespace because it should provide nice and intentional alias.
+ *
+ * How to use: When using as class member
+ * 1. Define dtor in cpp file. (IMPORTNAT)
+ * 2. Predeclare MyExampleClazz e.g. class MyExampleClazz;
+ * 3. E.g. Pimpl<MyExampleClazz> _dataSource;
+ * 4. In cpp file include file with definition of MyExampleClazz
+ */
+
 template <class T>
 using Pimpl = std::unique_ptr<T>;
